aef6f1596e32b2cbcfdf0985c3ef51c082bef1ac
mohammadpirian <mohammadpiriyan@gmail.com>
2026-08-25 16:14:39 +0330

    chore( StepProgressBar): update style

--- components/ProgressBar/StepProgressBar.tsx
+++ components/ProgressBar/StepProgressBar.tsx
@@ -20,14 +20,13 @@ const StepProgressBar: FC<PropsType> = ({ totalSteps, currentStep }) => {
           bgColor =
             "bg-gradient-to-r from-emerald-100 via-emerald-500 to-emerald-100";
         } else if (isCurrent) {
-          bgColor =
-            "bg-gradient-to-r from-fuchsia-100 via-fuchsia-500 to-fuchsia-100";
+          bgColor = "bg-gradient-to-r from-teal-50 via-teal-300 to-teal-50";
         }
 
         return (
           <div
             key={index}
-            className={`relative flex-1 h-2 rounded-full  transition-all duration-300 ${bgColor}`}
+            className={`relative flex-1 h-2  rounded-full  transition-all duration-300 ${bgColor}`}
           >
             {isCompleted && (
               <span className="absolute min-w-8 w-8 h-8 inset-1/2 translate-x-1/2 -top-3 flex items-center justify-center rounded-full bg-emerald-500 shadow-sm">
